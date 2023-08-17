1b4561134f087a110bf0c229fce99f907c1f47e6
ajfred700 <jose70020@gmail.com>
2023-08-17 16:30:11 -0600

    This is a new commit for what I originally planned to be amended

---
+++ src/App.js
@@ -0,0 +1,157 @@
+import { useState } from 'react';
+import { v4 as uuid} from 'uuid';
+import './App.css';
+import Header from "./componentes/Header/Header"
+import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
+import Footer from './componentes/Footer';
+
+function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
+  const [colaboradores, actualizarColaboradores] = useState([
+    {
+      id:uuid(),
+      equipo: "Programación",
+      foto: "https://github.com/ajfred700.png",
+      nombre: "José Alfredo",
+      puesto: "Desarrollador",
+      fav:true
+    },
+    {
+      id:uuid(),
+      equipo: "Programación",
+      foto: "https://github.com/ajfred700.png",
+      nombre: "Gustavo Adolfo",
+      puesto: "Director",
+      fav:false
+    }
+  ])
+  const [equipos, actualizarEquipos] = useState([
+    {
+      id:uuid(),
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      id:uuid(),
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      id:uuid(),
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      id:uuid(),
+      titulo: "Devops",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      id:uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      id:uuid(),
+      titulo: "Móvil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      id:uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    }
+  ]);
+  //Ternario --> condicion ? seMuestra : noSeMuestra
+  // condicion && seMuestra
+ 
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //Registrar colaborador
+
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
+    //Spread operator
+    actualizarColaboradores([...colaboradores, colaborador])
+  }
+  //Eliminar colaborador
+  const eliminarColaborador = (id) => {
+    console.log("Eliminar colaborador", id)
+    const nuevosColaboradores = colaboradores.filter((colaboradores) => colaboradores.id !==id)
+    actualizarColaboradores(nuevosColaboradores)
+  }
+  //Actualizar Color
+  const actualizarColor = (color,id) => {
+    console.log("cambiar color",color,id)
+    const equiposActualizados = equipos.map((equipo) =>{
+      if(equipo.id === id){
+        equipo.colorPrimario = color
+      }
+
+      return equipo
+    })
+
+    actualizarEquipos(equiposActualizados)
+  } 
+
+  // crear nuevo equipo
+  const crearEquipo =(nuevoEquipo) => { 
+    console.log(nuevoEquipo)
+    actualizarEquipos([...equipos, {...nuevoEquipo,id: uuid}])  
+  }
+  
+const like = (id) => {
+      console.log(id);
+      const colaboradoresActualizados = colaboradores.map((colaborador) => {
+        if (colaborador.id=== id) {
+          colaborador.fav = !colaborador.fav
+        }
+        return colaborador;
+  })
+  actualizarColaboradores(colaboradoresActualizados)
+}
+  //Lista de equipos
+  
+  return (
+    <div>
+      <Header />
+      {/* {mostrarFormulario ? <Formulario /> : <></>} */}
+      {
+        mostrarFormulario && <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+          crearEquipo={crearEquipo}
+        />
+      }
+
+      <MiOrg cambiarMostrar={cambiarMostrar} />
+
+      {
+        equipos.map((equipo) => <Equipo
+          datos={equipo}
+          key={equipo.titulo}
+          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
+          like={like}
+        />
+        )
+      }
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
